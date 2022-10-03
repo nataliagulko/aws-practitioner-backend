eb678f3c820b3301ee87a28c7f1059364ff5ab7b
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-10-03 14:49:22 +0300

    feat(product): get from db

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -1,13 +1,35 @@
 import { formatJSONResponse } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
-import { PRODUCTS_MOCK } from "@mocks/products";
 import { APIGatewayEvent } from "aws-lambda";
+import * as AWS from "aws-sdk";
 
-const getProductsById = async (event: APIGatewayEvent) =>
-  formatJSONResponse({
-    product: PRODUCTS_MOCK.find(
-      (product) => product.id === Number(event.pathParameters.productId)
-    ),
-  });
+const db = new AWS.DynamoDB.DocumentClient();
+
+const getProduct = async (id: string) =>
+  await db
+    .get({
+      TableName: process.env.PRODUCTS_TABLE_NAME,
+      Key: {
+        id,
+      },
+    })
+    .promise();
+
+const getStockProduct = async (id: string) =>
+  await db
+    .get({
+      TableName: process.env.STOCKS_TABLE_NAME,
+      Key: {
+        product_id: id,
+      },
+    })
+    .promise();
+
+const getProductsById = async (event: APIGatewayEvent) => {
+  const id = String(event.pathParameters.productId);
+  const { Item: product = {} } = await getProduct(id);
+  const { Item: stockProduct = {} } = await getStockProduct(id);
+  return formatJSONResponse({ ...product, count: stockProduct.count });
+};
 
 export const main = middyfy(getProductsById);
