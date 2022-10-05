c9b0ea99443c4b4b7b43f6d85771c4375a6f5715
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-10-05 17:04:07 +0300

    refactor(import-service): importProductsFile

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,7 +1,8 @@
 import { middyfy } from "@libs/lambda";
 import { APIGatewayEvent } from "aws-lambda";
-import { formatJSONResponse } from "../../libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
 import * as AWS from "aws-sdk";
+import { log } from "@utils/log";
 
 const s3Client = new AWS.S3();
 
@@ -19,7 +20,7 @@ const getSignedURL = async (
 };
 
 const importProductsFile = async (event: APIGatewayEvent) => {
-  console.log("importProductsFile event", event);
+  log(event);
   const name: string | undefined = event.queryStringParameters?.name;
   if (name) {
     const url = await getSignedURL(process.env.IMPORT_BUCKET, name, 60);
