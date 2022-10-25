bfb2ee33b32afeedd88c00c482738c6c9eecc807
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-10-25 15:41:04 +0300

    feat(product-service): add sql scripts

---
+++ product-service/src/db/insert_products.sql
@@ -0,0 +1,7 @@
+INSERT INTO products
+(id, title, description, price)
+VALUES(uuid_generate_v4(), 'Reg', 'From Made in Abyss, a figure of Reg is here.', 1200),
+(uuid_generate_v4(), 'Nanachi', 'From Made in Abyss, a figure of Nanachi is here.', 1200),
+(uuid_generate_v4(), 'Mitty', 'From Made in Abyss, a figure of Mitty is here.', 1200),
+(uuid_generate_v4(), 'Riko', 'From Made in Abyss, a figure of Riko is here.', 1200),
+(uuid_generate_v4(), 'Prushka', 'From Made in Abyss, a figure of Prushka is here.', 1200);
---
+++ product-service/src/db/insert_stocks.sql
@@ -0,0 +1,2 @@
+INSERT INTO stocks(product_id, count)
+	select id, 5 from products;
