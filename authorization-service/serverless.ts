caa365331712bf3eeb76a07fb19fe64e83ad7a6b
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-11-15 09:25:28 +0300

    fix: remove creds from config

--- authorization-service/serverless.ts
+++ authorization-service/serverless.ts
@@ -18,7 +18,6 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
-      nataliagulko: "TEST_PASSWORD",
     },
   },
   resources: {
